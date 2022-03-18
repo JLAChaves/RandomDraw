cfe22714bacfa356c86962cf9de7ddf470c408bb
JLAChaves <lucas2015nxi@gmail.com>
2022-03-18 15:42:44 -0400

    Added RaffleController with method Create

---
+++ RandomDraw/Controllers/RaffleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomDraw.Controllers
+{
+    public class RaffleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
