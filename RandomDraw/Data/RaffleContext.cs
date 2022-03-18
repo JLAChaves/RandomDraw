a3ad5e2e073135fe000c01d3f6dfdb1a0c4f06a6
JLAChaves <lucas2015nxi@gmail.com>
2022-03-18 15:19:12 -0400

    Folder Models and Data created

---
+++ RandomDraw/Data/RaffleContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RandomDraw.Data
+{
+    public class RaffleContext : DbContext
+    {
+        public RaffleContext(DbContextOptions<RaffleContext> options) : base(options)
+        {
+        }
+
+
+    }
+
+
+
+}
---
+++ RandomDraw/Models/Raffle.cs
@@ -0,0 +1,11 @@
+﻿namespace RandomDraw.Models
+{
+    public class Raffle
+    {
+        public int Id { get; set; }
+        public int SmallestNumber { get; set; }
+        public int HigherNumber { get; set; }
+        public int AmountOfNumbers { get; set; }
+        public string? Result { get; set; }
+    }
+}
